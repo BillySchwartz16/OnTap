11fed589930c6228085df6d959adef17756567e7
Schwartz, Billy <Billy.Schwartz@sscinc.com>
2022-12-15 21:48:32 -0500

    created first endpoint

---
+++ src/react/app/src/Hello.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Hello() {
+  const [hello, setHello] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get('http://localhost:80/');
+      setHello(res.data.Hello);
+    }
+    fetchData();
+  }, []);
+
+  return <h1>{hello}</h1>;
+}
+
+export default Hello;
